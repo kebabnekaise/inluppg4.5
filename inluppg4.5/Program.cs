ac9357b42878da1d9d0435466c47382967b9da79
emsi06001 <emsi06001@E-PW0234N3>
2023-10-18 13:09:08 +0200

    Add project files.

---
+++ inluppg4.5/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.Runtime.InteropServices;
+
+namespace inluppg4_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Skriv ett tal");
+            string inputNum = Console.ReadLine();
+            string outputNum = inputNum;
+
+            for (int i = 0; i < inputNum.Length; i++)
+            {
+                int currentNum = int.Parse(inputNum[i].ToString());
+                if (currentNum == 9)
+                {
+                    Console.Write(0);
+                }
+                else
+                {
+                    currentNum += 1;
+                    Console.Write(currentNum);
+                }
+            }
+        }
+    }
+}
